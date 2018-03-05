813443e3ece09a9d386188dcbfb93501071e8002
Ren Haojie <renhaojie520@live.cn>
2018-03-05 23:22:10 +0800

    commit

--- coordinateconvert.js
+++ coordinateconvert.js
@@ -1,5 +1,8 @@
 var renderer;
 var camera;
+var cube;
+var sphere;
+var cylinder;
 function init() {
   var scene = new THREE.Scene();
 
@@ -22,17 +25,22 @@ function init() {
   addPlane(scene);
   addCube(scene);
   addCylinder(scene);
+  addSphere(scene);
   update(scene, camera, renderer);
 }
+
 function addSpotLight(scene) {
   var light = new THREE.SpotLight(0xffffff);
   light.position.set(-40, 60, -10);
 
   scene.add(light);
 }
+
 function addPlane(scene) {
   var planeGeo = new THREE.PlaneGeometry(200, 200, 40, 40);
-  var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  var planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xffffff
+  });
   planeMaterial.map = new THREE.TextureLoader().load(
     "textures/camo_cloth_multicam_2048.jpg"
   );
@@ -41,44 +49,93 @@ function addPlane(scene) {
   plane.position.x = 15;
   scene.add(plane);
 }
+
 function addCube(scene) {
   var cubeGeo = new THREE.BoxGeometry(4, 4, 4);
-  var cubeMat = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-  var cube = new THREE.Mesh(cubeGeo, cubeMat);
+  var cubeMat = new THREE.MeshLambertMaterial({
+    color: 0xff0000
+  });
+  cube = new THREE.Mesh(cubeGeo, cubeMat);
 
   cube.position.set(-10, 2, 0);
   cube.name = "cube";
   scene.add(cube);
 }
+
 function addCylinder(scene) {
   var cylinderGeo = new THREE.CylinderGeometry(2, 2, 20);
-  var cylinderMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  var cylinderMat = new THREE.MeshLambertMaterial({
+    color: 0xffffff
+  });
   cylinderMat.map = new THREE.TextureLoader().load("textures/marble.jpg");
-  var cylinder = new THREE.Mesh(cylinderGeo, cylinderMat);
-
+  cylinder = new THREE.Mesh(cylinderGeo, cylinderMat);
+  cylinder.name = "cylinder";
   scene.add(cylinder);
 }
 
 function addSphere(scene) {
   var sphereGeo = new THREE.SphereGeometry(4, 20);
-  var sphereMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  var sphereMat = new THREE.MeshLambertMaterial({
+    color: 0xffffff
+  });
   sphereMat.map = new THREE.TextureLoader().load(
     "textures/wood_1-1024x1024.png"
   );
-  var sphere = new THREE.Mesh(sphereGeo, sphereMat);
+  sphere = new THREE.Mesh(sphereGeo, sphereMat);
   sphere.position.x = 10;
+  sphere.position.y = 2;
   sphere.name = "sphere";
   scene.add(sphere);
 }
+// var projector = new THREE.Projector();
+function onDocumentMouseDown(event) {
+  var vector = new THREE.Vector3(
+    event.clientX / window.innerWidth * 2 - 1,
+    -(event.clientY / window.innerHeight) * 2 + 1,
+    0.5
+  );
+
+  vector.unproject(camera);
+
+  var rayCaster = new THREE.Raycaster(
+    camera.position,
+    vector.sub(camera.position).normalize()
+  );
+
+  var intersects = rayCaster.intersectObjects([sphere, cylinder, cube]);
+
+  if (intersects.length > 0) {
+    var material = intersects[0].object.material;
+    material.transparent = true;
+    if (material.opacity === 0.5) {
+      material.opacity = 1;
+    } else {
+      material.opacity = 0.5;
+    }
+  }
+}
+document.addEventListener("mousedown", onDocumentMouseDown, false);
+var step = 0;
 
 function update(scene, camera, renderer) {
   renderer.render(scene, camera);
 
+  if (cube) {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+  }
+  step += 0.05;
+  if (sphere) {
+    sphere.position.x = 20 + 10 * Math.cos(step);
+    sphere.position.y = 2 + 10 * Math.abs(Math.sin(step));
+  }
   requestAnimationFrame(function() {
     update(scene, camera, renderer);
   });
 }
 window.addEventListener("resize", onResize, false);
+
 function onResize() {
   renderer.setSize(window.innerWidth, window.innerhwi);
 
